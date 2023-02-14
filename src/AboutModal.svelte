8d976de7d7ca104f3b3bdc1fc28aac0ae026a135
Vanya A. Sergeev <v@sergeev.io>
2023-02-13 22:18:53 -0600

    add license information to about modal

--- src/AboutModal.svelte
+++ src/AboutModal.svelte
@@ -22,6 +22,15 @@
       <span class="font-semibold">briefsky</span> is a free weather frontend to a variety of weather providers, with an interface reminiscent of the late Dark Sky.
       briefsky does not provide weather data — a weather provider must be configured in the settings.
     </p>
+    <br />
+    <p class="text-justify [&_a]:underline">
+      briefsky is <a href="https://github.com/vsergeev/briefsky/blob/master/LICENSE">MIT licensed</a>. In addition, briefsky uses Erik Flower's Weather icons (<a
+        href="https://scripts.sil.org/cms/scripts/page.php?site_id=nrsi&id=OFL">Open Font License</a
+      >), as well as miscellaneous icons from Radix Icons (<a href="https://github.com/radix-ui/icons/blob/master/LICENSE">MIT License</a>), Material Design
+      Icons (<a href="https://github.com/Templarian/MaterialDesign/blob/master/LICENSE">Apache 2.0 License</a>), and MingCute Icons (<a
+        href="https://github.com/Richard9394/MingCute/blob/main/LICENSE">Apache 2.0 License</a
+      >).
+    </p>
   </div>
 
   <svelte:fragment slot="footer">
