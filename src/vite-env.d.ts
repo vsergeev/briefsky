06962f3d807bb81c2212b62ab26446686cdb1343
Vanya A. Sergeev <v@sergeev.io>
2023-02-24 05:38:19 -0600

    add package version and commit id global constants

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,11 @@
 /// <reference types="svelte" />
 /// <reference types="vite/client" />
+
+declare global {
+  interface Window {
+    __APP_VERSION__: string;
+    __APP_COMMIT_ID__: string;
+  }
+}
+
+export {};
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
+import * as child_process from 'child_process';
+
+/* Get commit id */
+let commitId: string;
+try {
+  commitId = child_process.execSync('git rev-parse --short HEAD 2>/dev/null').toString();
+} catch {
+  commitId = 'unknown';
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '',
+  define: {
+    'window.__APP_VERSION__': JSON.stringify(process.env.npm_package_version),
+    'window.__APP_COMMIT_ID__': JSON.stringify(commitId),
+  },
   plugins: [svelte()],
 });
