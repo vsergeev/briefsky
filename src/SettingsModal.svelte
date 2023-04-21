707ebb1c9b793a01758fb1e64383357e7a28d4b8
Vanya A. Sergeev <v@sergeev.io>
2023-04-21 01:29:47 -0500

    add category tabs to settings modal

--- src/SettingsModal.svelte
+++ src/SettingsModal.svelte
@@ -6,7 +6,7 @@
   import { Location } from './providers/Location';
   import { Units, decodeConfiguration, encodeConfiguration } from './Configuration';
 
-  import { Modal, Label, Select, Input, Hr, Button, ButtonGroup, Spinner, Radio } from 'flowbite-svelte';
+  import { Modal, Tabs, TabItem, Label, Select, Input, Hr, Button, ButtonGroup, Spinner, Radio } from 'flowbite-svelte';
   import Icon from '@iconify/svelte';
 
   enum LocationMode {
@@ -96,78 +96,95 @@
 </script>
 
 <Modal bind:open={modalOpen} title="Settings" class="w-full sm:w-2/3">
-  <div class="space-y-4">
-    <div>
-      <Label for="select-provider" class="mb-2">Weather Provider</Label>
-      <Select
-        id="select-provider"
-        items={ProviderFactories.map((provider) => ({ name: provider.description, value: provider }))}
-        bind:value={providerFactory}
-        on:change={updateProviderParams}
-        placeholder="Select Provider"
-      />
-    </div>
+  <div>
+    <Tabs contentClass="mt-6">
+      <TabItem open title="General">
+        <div class="space-y-4">
+          <div>
+            <Label for="select-provider" class="mb-2">Weather Provider</Label>
+            <Select
+              id="select-provider"
+              items={ProviderFactories.map((provider) => ({ name: provider.description, value: provider }))}
+              bind:value={providerFactory}
+              on:change={updateProviderParams}
+              placeholder="Select Provider"
+            />
+          </div>
 
-    {#if providerFactory && providerFactory.fields.length > 0}
-      {#each providerFactory.fields as field}
-        <div>
-          <Label for="input-{field.name}" class="mb-2">{field.description}</Label>
-          <Input id="input-{field.name}" bind:value={providerParams[field.name]} required />
+          {#if providerFactory && providerFactory.fields.length > 0}
+            {#each providerFactory.fields as field}
+              <div>
+                <Label for="input-{field.name}" class="mb-2">{field.description}</Label>
+                <Input id="input-{field.name}" bind:value={providerParams[field.name]} required />
+              </div>
+            {/each}
+          {/if}
+
+          <Hr class="mt-2" height="h-px" />
+
+          {#if providerFactory.requiresLocation}
+            <div>
+              <Label for="radio-location-mode" class="mb-2">Location</Label>
+              <div class="flex gap-4 ml-2 my-5">
+                <Radio id="radio-location-mode" bind:group={locationMode} value={LocationMode.Geolocation}>Geolocation</Radio>
+                <Radio bind:group={locationMode} value={LocationMode.Coordinates}>Coordinates</Radio>
+              </div>
+            </div>
+
+            <div on:paste={handlePaste}>
+              <Label for="group-location" class="mb-2">Coordinates</Label>
+              <ButtonGroup id="group-location" class="w-full">
+                <Input
+                  id="input-latitude"
+                  bind:value={location.latitude}
+                  disabled={locationMode === LocationMode.Geolocation}
+                  placeholder="Latitude (decimal)"
+                />
+                <Input
+                  id="input-longitude"
+                  bind:value={location.longitude}
+                  disabled={locationMode === LocationMode.Geolocation}
+                  placeholder="Longitude (decimal)"
+                />
+                <Button
+                  id="btn-locate"
+                  on:click={handleLocate}
+                  disabled={locationMode === LocationMode.Geolocation || locationLoading}
+                  size="sm"
+                  outline
+                  class="!p-3"
+                  color="light"
+                >
+                  {#if locationLoading}
+                    <Spinner size="5" color="gray" />
+                  {:else}
+                    <Icon icon="radix-icons:crosshair-2" class="text-lg" />
+                  {/if}
+                </Button>
+              </ButtonGroup>
+            </div>
+          {/if}
+
+          <div>
+            <Label for="select-units" class="mb-2">Units</Label>
+            <Select
+              id="select-units"
+              items={[
+                { name: 'Imperial', value: Units.Imperial },
+                { name: 'Metric', value: Units.Metric },
+              ]}
+              bind:value={units}
+            />
+          </div>
+
+          <div>
+            <Label for="input-title" class="mb-2">Title (optional)</Label>
+            <Input id="input-title" bind:value={title} required />
+          </div>
         </div>
-      {/each}
-    {/if}
-
-    <Hr class="mt-2" height="h-px" />
-
-    {#if providerFactory.requiresLocation}
-      <div>
-        <Label for="radio-location-mode" class="mb-2">Location</Label>
-        <div class="flex gap-4 ml-2 my-5">
-          <Radio id="radio-location-mode" bind:group={locationMode} value={LocationMode.Geolocation}>Geolocation</Radio>
-          <Radio bind:group={locationMode} value={LocationMode.Coordinates}>Coordinates</Radio>
-        </div>
-      </div>
-
-      <div on:paste={handlePaste}>
-        <Label for="group-location" class="mb-2">Coordinates</Label>
-        <ButtonGroup id="group-location" class="w-full">
-          <Input id="input-latitude" bind:value={location.latitude} disabled={locationMode === LocationMode.Geolocation} placeholder="Latitude (decimal)" />
-          <Input id="input-longitude" bind:value={location.longitude} disabled={locationMode === LocationMode.Geolocation} placeholder="Longitude (decimal)" />
-          <Button
-            id="btn-locate"
-            on:click={handleLocate}
-            disabled={locationMode === LocationMode.Geolocation || locationLoading}
-            size="sm"
-            outline
-            class="!p-3"
-            color="light"
-          >
-            {#if locationLoading}
-              <Spinner size="5" color="gray" />
-            {:else}
-              <Icon icon="radix-icons:crosshair-2" class="text-lg" />
-            {/if}
-          </Button>
-        </ButtonGroup>
-      </div>
-    {/if}
-
-    <div>
-      <Label for="select-units" class="mb-2">Units</Label>
-      <Select
-        id="select-units"
-        items={[
-          { name: 'Imperial', value: Units.Imperial },
-          { name: 'Metric', value: Units.Metric },
-        ]}
-        bind:value={units}
-      />
-    </div>
-
-    <div>
-      <Label for="input-title" class="mb-2">Title (optional)</Label>
-      <Input id="input-title" bind:value={title} required />
-    </div>
+      </TabItem>
+      <TabItem title="View" />
+    </Tabs>
   </div>
 
   <svelte:fragment slot="footer">
