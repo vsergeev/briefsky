18c2ea17ce0dc24a434d6713befd9b6a9259c9dc
Vanya A. Sergeev <v@sergeev.io>
2023-09-15 23:56:05 -0500

    export conversion functions from Amount component

--- src/components/scalars/Amount.svelte
+++ src/components/scalars/Amount.svelte
@@ -1,11 +1,13 @@
+<script context="module" lang="ts">
+  export function mmToIn(amount: number): number {
+    return amount * 0.0393700787;
+  }
+</script>
+
 <script lang="ts">
   import { configuration, Units } from '../../Configuration';
 
   export let value: number = 0;
-
-  function mmToIn(amount: number): number {
-    return amount * 0.0393700787;
-  }
 </script>
 
 {#if $configuration.units === Units.Imperial}
