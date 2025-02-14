2a4639c43d8ca599b29f6b71856205be3e168ed5
Vanya A. Sergeev <v@sergeev.io>
2025-02-14 01:41:27 -0600

    simulate additional behaviors in forwardGeocode() in ExampleGeocoder

--- src/geocoders/ExampleGeocoder.ts
+++ src/geocoders/ExampleGeocoder.ts
@@ -5,13 +5,23 @@ export class ExampleGeocoder implements Geocoder {
   static description = 'Example';
   static attribution = undefined;
 
-  async forwardGeocode(_: string): Promise<LocationCandidate[]> {
+  async forwardGeocode(query: string): Promise<LocationCandidate[]> {
+    // Simulate request time
+    await new Promise((resolve) => setTimeout(resolve, 750));
+
+    if (query === 'nowhere') return [];
+
+    if (query === 'error') throw new Error(`Fetching from ExampleGeocoder: Unexpected error`);
+
     return [
       { name: 'New York, New York', location: new Location('40.7127281', '-74.0060152') },
       { name: 'Los Angeles, California', location: new Location('34.0536909', '-118.242766') },
       { name: 'Chicago, Illinois', location: new Location('41.8755616', '-87.6244212') },
       { name: 'Houston, Texas', location: new Location('29.7589382', '-95.3676974') },
       { name: 'Phoenix, Arizona', location: new Location('33.4484367', '-112.074141') },
+      { name: 'Philadelphia, Pennsylvania', location: new Location('39.9527237', '-75.1635262') },
+      { name: 'San Antonio, Texas', location: new Location('29.4246002', '-98.4951405') },
+      { name: 'San Diego, California', location: new Location('32.7174202', '-117.162772') },
     ];
   }
 
